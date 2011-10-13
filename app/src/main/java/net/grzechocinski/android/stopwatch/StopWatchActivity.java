f840e17f9ab367c8d00a527a3f6749b802a0eddd
Mateusz Grzechociński <mateusz.grzechocinski@javart.eu>
2011-10-13 18:50:21 +0200

    Created basic layout. No binding

--- app/src/main/java/net/grzechocinski/android/stopwatch/StopWatchActivity.java
+++ app/src/main/java/net/grzechocinski/android/stopwatch/StopWatchActivity.java
@@ -2,6 +2,9 @@ package net.grzechocinski.android.stopwatch;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 
 /**
@@ -13,5 +16,31 @@ public class StopWatchActivity extends Activity {
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.count);
+
+		Button startButton = (Button) findViewById(R.id.button_start);
+		Button stopButton = (Button) findViewById(R.id.button_stop);
+		Button resetButton = (Button) findViewById(R.id.button_reset);
+
+		startButton.setOnClickListener(new View.OnClickListener() {
+			public void onClick(View view) {
+				showToast(R.string.button_start);
+			}
+		});
+
+		stopButton.setOnClickListener(new View.OnClickListener() {
+			public void onClick(View view) {
+				showToast(R.string.button_stop);
+			}
+		});
+
+		resetButton.setOnClickListener(new View.OnClickListener() {
+			public void onClick(View view) {
+				showToast(R.string.button_reset);
+			}
+		});
+	}
+
+	private void showToast(int msgResId){
+		Toast.makeText(StopWatchActivity.this, msgResId,Toast.LENGTH_SHORT).show();
 	}
 }
